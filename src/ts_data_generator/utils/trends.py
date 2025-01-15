2493cb060de0576b11d02dd72c131c3c213b1304
Manoj Manivannan <manojm18@live.in>
2025-01-15 20:12:20 +0000

    clean up

--- src/ts_data_generator/utils/trends.py
+++ src/ts_data_generator/utils/trends.py
@@ -104,40 +104,6 @@ class SinusoidalTrend(Trends):
 
         return sinusoidal_wave
     
-    # def generate(self, timestamps) -> np.ndarray:
-    #     """
-    #     Generate a sinusoidal wave with added noise.
-        
-    #     Parameters:
-    #         amplitude (float): Amplitude of the sinusoidal wave.
-    #         freq (float): Frequency of the sinusoidal wave.
-    #         phase (float): Phase offset of the sinusoidal wave.
-    #         noise_level (float): Standard deviation of the noise.
-            
-    #     Returns:
-    #         y (numpy.ndarray): Sinusoidal wave with noise.
-    #     """
-
-    #     # frequency is in hours, phase is in minutes,
-    #     # convert frequency to period
-    #     frequency = 1/self._freq  # Set frequency to oscillate once every hour
-    #     phase_hours = self._phase / 60  # Convert phase to hours
-    #     noise_level = self._noise_level * 0.1 # Add 10% noise
-
-    #     base_wave = np.zeros(len(timestamps))
-
-    #     # Convert phase from minutes to hours
-        
-    #     base_wave += self._amplitude *np.sin(2 * np.pi * frequency * (timestamps.hour + timestamps.minute / 60)) + phase_hours
-
-    
-    #     noise = np.random.normal(0, noise_level, len(timestamps))  # Generate noise
-    #     base_wave += noise  # Add noise to the wave
-        
-    #     # Add noise to the wave
-    #     y = base_wave #+ noise
-        
-    #     return y
     
 
 class LinearTrend(Trends):
